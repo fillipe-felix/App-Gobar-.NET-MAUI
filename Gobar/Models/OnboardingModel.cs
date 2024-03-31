7bb4226ede13d595cdbea319a6e26a7a3768bd54
fillipe.felix <felipesoares_1993@hotmail.com>
2024-03-31 18:59:00 -0300

    iniciando tela de onboarding

---
+++ Gobar/Models/OnboardingModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Gobar.Models;
+
+public class OnboardingModel
+{
+    public string ImageSource { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
---
+++ Gobar/ViewModels/OnboardingPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Gobar.Models;
+
+namespace Gobar.ViewModels;
+
+public partial class OnboardingPageViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<OnboardingModel> _carouselItems = new ObservableCollection<OnboardingModel>();
+
+    public OnboardingPageViewModel()
+    {
+        CarouselItems.Add(new OnboardingModel
+        {
+            ImageSource = "picture_on_1.png", 
+            Title = "Welcome Gobars", 
+            Description = "Find the best grooming experience in your city with just one tap! Don't miss out on the haircut or treatment of your dreams. Let's start now!"
+        });
+        CarouselItems.Add(new OnboardingModel
+        {
+            ImageSource = "picture_on_2.png", 
+            Title = "Loooking for barber?", 
+            Description = "Find the best barbershop around you in seconds, make an appointment, and enjoy the best grooming experience."
+        });
+        CarouselItems.Add(new OnboardingModel
+        {
+            ImageSource = "picture_on_3.png", 
+            Title = "Everything in your hands", 
+            Description = "With Gobar, find high-quality barbershops, see reviews, and make appointments easily. Achieve your confident appearance!"
+        });
+    }
+}
