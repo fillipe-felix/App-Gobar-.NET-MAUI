de40c8d61692c40d3adc1522caa2a413a0f6d32d
fillipe.felix <felipesoares_1993@hotmail.com>
2024-04-07 17:20:56 -0300

    Criação da tela Authentication

---
+++ Gobar/Views/Authentication.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Gobar.Views;
+
+public partial class Authentication : ContentPage
+{
+    public Authentication()
+    {
+        InitializeComponent();
+        
+    }
+}
+
