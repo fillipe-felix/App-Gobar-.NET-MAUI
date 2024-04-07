6ade737439efd16ab0719d80cf1f671d07a53ab7
fillipe.felix <felipesoares_1993@hotmail.com>
2024-04-07 15:57:56 -0300

    Finalizando a tela de login

--- Gobar/App.xaml.cs
+++ Gobar/App.xaml.cs
@@ -1,11 +1,51 @@
-﻿namespace Gobar;
+﻿#if IOS
+using UIKit;
+using Foundation;
+#endif
+
+#if ANDROID
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+#endif
+
+namespace Gobar;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
+        RemoveBorders();
 
         MainPage = new AppShell();
     }
+
+    public void RemoveBorders()
+    {
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("Borderless", (handler, view) =>
+        {
+#if ANDROID
+            handler.PlatformView.Background = null;
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+#elif IOS
+            handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+            handler.PlatformView.Layer.BorderWidth = 0;
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+        });
+
+        Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping("Borderless", (handler, view) =>
+        {
+#if ANDROID
+            handler.PlatformView.Background = null;
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+#elif IOS
+            handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+            handler.PlatformView.Layer.BorderWidth = 0;
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+        });
+    }
+    
 }
---
+++ Gobar/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gobar.Views;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+    
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("", "Hello World", "Cancel");
+    }
+}
+
