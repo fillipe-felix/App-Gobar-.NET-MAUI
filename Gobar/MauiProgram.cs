63ea993f44fb300404747a615ce33f2ded907a84
fillipe.felix <felipesoares_1993@hotmail.com>
2024-04-07 11:41:03 -0300

    finalização de customização da tela de OnboardingPage.xaml

--- Gobar/MauiProgram.cs
+++ Gobar/MauiProgram.cs
@@ -11,8 +11,9 @@ public static class MauiProgram
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("PlusJakartaSans-Regular.ttf", "PlusJakartaSansRegular");
+                fonts.AddFont("PlusJakartaSans-Medium.ttf", "PlusJakartaSansMedium");
+                fonts.AddFont("PlusJakartaSans-Bold.ttf", "PlusJakartaSansBold");
             });
 
 #if DEBUG
